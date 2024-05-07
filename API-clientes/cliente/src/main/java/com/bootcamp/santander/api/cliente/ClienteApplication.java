712c71ec4dcf8c3e20698dcfe0cb7027e1366543
Magno De Oliveira <magnogtdb@hotmail.com>
2024-05-07 19:45:33 -0300

    implementacao da api de clientes

---
+++ API-clientes/cliente/src/main/java/com/bootcamp/santander/api/cliente/ClienteApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.santander.api.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApplication.class, args);
+	}
+
+}
