f58193a01d6b2d2ca257b3d5860de146c2a9c675
Alinebaldin <baldin.lilith@gmail.com>
2024-05-10 21:03:18 -0300

    implementação da api formulario de contratação

---
+++ API-formwork/formwork/src/main/java/com/bootcamp/santander/api/formwork/FormworkApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.santander.api.formwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormworkApplication.class, args);
+	}
+
+}
