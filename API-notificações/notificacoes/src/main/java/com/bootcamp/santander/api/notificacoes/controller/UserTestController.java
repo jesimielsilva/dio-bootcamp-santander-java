28570cd8db71b7cc1739f6e32f644ee9bf18ec1c
Everton <evertonpontesp@gmail.com>
2024-05-18 19:48:37 -0300

    Implementacao dos metodos getAllUsers createUser deleteUser

--- API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/controller/UserTestController.java
+++ API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/controller/UserTestController.java
@@ -1,7 +1,38 @@
 package com.bootcamp.santander.api.notificacoes.controller;
 
-public class UserTestController {
+import com.bootcamp.santander.api.notificacoes.entity.User;
+import com.bootcamp.santander.api.notificacoes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserTestController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletUser(@PathVariable Long id) {
+        if (userService.getUserById(id).isPresent()) {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
 
 
