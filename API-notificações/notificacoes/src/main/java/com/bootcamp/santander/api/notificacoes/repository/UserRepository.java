d2db9c0eebbe636a0f054ad18f83ef897f22ceef
Gabriel_GDSF <Gabriel_GDSF7@outlook.com>
2024-05-20 11:03:24 -0300

    Update do repositório

--- API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/repository/UserRepository.java
+++ API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/repository/UserRepository.java
@@ -1,3 +1,7 @@
 package com.bootcamp.santander.api.notificacoes.repository;
-public interface UserRepository {
+import com.bootcamp.santander.api.notificacoes.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
--- API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/service/UserService.java
+++ API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/service/UserService.java
@@ -1,4 +1,29 @@
 package com.bootcamp.santander.api.notificacoes.service;
+
+import com.bootcamp.santander.api.notificacoes.entity.User;
+import com.bootcamp.santander.api.notificacoes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    //método para obter uma lista de todos os usuários:
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    //método para a criação de um novo usuário:
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    //método para excluir um usuário informando o ID:
+    public void deleteUser(Long userId){
+        userRepository.deleteById(userId);
+    }
 }
