036efe19f78d98f11990a76a2e4bbc72f181c02a
Gabriel_GDSF <Gabriel_GDSF7@outlook.com>
2024-05-20 13:09:43 -0300

    Implementação do UserService

--- API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/service/UserService.java
+++ API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/service/UserService.java
@@ -1,4 +1,27 @@
 package com.bootcamp.santander.api.notificacoes.service;
+
+import com.bootcamp.santander.api.notificacoes.entity.User;
+import com.bootcamp.santander.api.notificacoes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
