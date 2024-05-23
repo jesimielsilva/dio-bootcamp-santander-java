8dc63aff01f50788e10ff20b6a92193ccdb24f69
Guilherme Santos <guilhermeag123@hotmail.com>
2024-05-23 19:20:30 -0300

    implementar a entity

--- API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/entity/User.java
+++ API-notificações/notificacoes/src/main/java/com/bootcamp/santander/api/notificacoes/entity/User.java
@@ -1,4 +1,20 @@
 package com.bootcamp.santander.api.notificacoes.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+
 
 }
